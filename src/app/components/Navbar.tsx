f4faad28a8812e801f3a71af15c11ca7b9b6809f
Sebas Echazú <sebastianechazu@outlook.com>
2024-11-27 14:48:21 -0300

    Agregar logo en la barra de navegación y mejorar la estructura del código

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Imagen from '../../img/Logo_CCI.jpeg';
 
 
 const Navbar: React.FC = () => {
@@ -10,13 +11,14 @@ const Navbar: React.FC = () => {
     <nav className="bg-white shadow-md fixed w-full z-50">
       <div className="w-full mx-0 px-1 sm:px-1 lg:px-2">
         <div className="flex justify-between items-center h-16">
-          
+
           {/* Logo leftside */}
           <div className="flex items-center flex-grow">
             <Link href="/">
-              <Image                    
-                src="/Logo_CCI.jpeg"
+              <Image
+                src={Imagen}	
                 alt="CCI"
+                typeof='webp'
                 width={350}
                 height={350}
                 className="object-contain max-h-16"
@@ -24,31 +26,31 @@ const Navbar: React.FC = () => {
               />
             </Link>
           </div>
-          
+
           {/* Links rightside */}
           <div className="flex items-center justify-end space-x-3">
             <Link href="/pages/about" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Quiénes somos
+              Quiénes somos
             </Link>
 
             <Link href="/pages/servicepage" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Servicios
+              Servicios
             </Link>
 
             <Link href="/pages/news" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Noticias
+              Noticias
             </Link>
 
             <Link href="/pages/calendar" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Calendario
+              Calendario
             </Link>
 
             <Link href="/pages/reports" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Informes
-            </Link>            
+              Informes
+            </Link>
 
             <Link href="/pages/partners" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
-            Socios
+              Socios
             </Link>
 
             {/* <Link href="/pages/login" className="no-underline decoration-4 transition-all duration-500 hover:underline hover:decoration-blue-400 uppercase">
